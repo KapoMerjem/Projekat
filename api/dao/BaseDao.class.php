bd9bc73117298a5a0601bb26a1480dd4562a01ff
KapoMerjem <61578763+KapoMerjem@users.noreply.github.com>
2021-03-18 21:57:40 +0100

    add query method

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -10,9 +10,8 @@ public function __construct(){
   try {
     $this->connection= new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
     $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected";
   } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    throw $e;
   }
 
 }
@@ -27,13 +26,16 @@ public function update(){
 
 }
 
-public function query(){
-// SELECT * FROM users WHERE id = 7;
+public function query($query, $params){
+  $stmt = $this->connection->prepare($query);
+  $stmt->execute($params);
+  return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 }
 
-public function query_unique(){
-
+public function query_unique($query, $params){
+  $results = $this->query($query, $params);
+  return reset($results);
 }
 
 }
--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -1,11 +1,16 @@
 <?php
+require_once dirname(__FILE__)."/BaseDao.class.php";
 
 class UserDao extends BaseDao{
 
-public function get_user_by_email($email){
+  public function get_user_by_email($email){
 
+    return $this->query_unique("SELECT * FROM users WHERE email = :email", ["email" => $email]);
 }
 
+public function get_user_by_user_id($user_id){
+  return $this->query_unique("SELECT * FROM users WHERE user_id = :id", ["id" => $user_id]);
+}
 }
 
  ?>
