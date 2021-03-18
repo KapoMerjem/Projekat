ce5da273812b222c9914576679391f31dca61d4a
KapoMerjem <61578763+KapoMerjem@users.noreply.github.com>
2021-03-18 22:27:33 +0100

    add insert method

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -11,6 +11,20 @@ class UserDao extends BaseDao{
 public function get_user_by_user_id($user_id){
   return $this->query_unique("SELECT * FROM users WHERE user_id = :id", ["id" => $user_id]);
 }
+
+public function add_user($user){
+  $sql = "INSERT INTO users (name, surname, email, phone_number, username, password) VALUES (:name, :surname, :email, :phone_number, :username, :password)";
+  $stmt = $this->connection->prepare($sql);
+  $stmt->execute($user);
+  $user['id'] = $this->connection->lastInsertId();
+  return $user;
+
+}
+
+public function update_user($user_id, $user){
+
+
+}
 }
 
  ?>
--- api/test.php
+++ api/test.php
@@ -7,7 +7,17 @@ error_reporting(E_ALL);
 require_once dirname(__FILE__)."/dao/UserDao.class.php";
 $user_dao = new UserDao();
 
-$user = $user_dao->get_user_by_user_id(3);
+//$user = $user_dao->get_user_by_user_id(3);
+
+$user1 = [
+"name" => "Sabina",
+"surname" => "Topić",
+"email" => "sabi@gmail.com",
+"phone_number" => "061245523",
+"username" => "SabinaT",
+"password" => "sabi123"
+];
+$user = $user_dao->add_user($user1);
 
 // $user_dao->get_user_by_email("mejakapo@gmail.com");
 
