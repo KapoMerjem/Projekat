17aac168502b8e3ebb2313f5cedc52f5bb632136
KapoMerjem <61578763+KapoMerjem@users.noreply.github.com>
2021-04-03 10:05:16 +0200

    user update

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__)."/BaseDao.class.php";
 class UserDao extends BaseDao{
 
   public function __construct(){
-    parent::__construct("bands");
+    parent::__construct("users");
   }
 
   public function get_user_by_user_id($user_id){
