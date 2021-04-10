c38719da13f70141d29e6211e4639294708b77c3
KapoMerjem <61578763+KapoMerjem@users.noreply.github.com>
2021-04-10 09:36:57 +0200

    search option on bands

--- api/dao/BandDao.class.php
+++ api/dao/BandDao.class.php
@@ -6,6 +6,14 @@ class BandDao extends BaseDao{
   public function __construct(){
     parent::__construct("bands");
   }
+
+  public function get_bands($search, $offset, $limit){
+    return $this->query("SELECT *
+                         FROM bands
+                         WHERE LOWER(name) LIKE CONCAT('%', :name, '%')
+                         LIMIT ${limit} OFFSET ${offset}", ["name" => strtlower($search)]);
+  }
+
 }
 
 ?>
--- api/routes/bands.php
+++ api/routes/bands.php
@@ -4,16 +4,22 @@ Flight::route('GET /bands', function(){
     $offset = Flight::query('offset', 0);
     $limit = Flight::query('limit', 10);
 
-    Flight::json(Flight::bandDao()->get_all($offset, $limit));
+    $search = Flight::query('search');
+
+    if($search){
+      Flight::json(Flight::bandDao()->get_bands($search, $offset, $limit));
+    }else{
+      Flight::json(Flight::bandDao()->get_all($offset, $limit));
+    }
 });
 
 Flight::route('GET /bands/@id', function($id){
-    Flight::json(Flight::bandDao->get_by_id($id));
+    Flight::json(Flight::bandDao()->get_by_id($id));
 });
 
 Flight::route('POST /bands', function(){
     $request = Flight::request()->data->getData();
-    Flight::json(Flight::bandDao->add($data));
+    Flight::json(Flight::bandDao()->add($data));
 });
 Flight::route('PUT /bands/@id', function($id){
     $request = Flight::request();
