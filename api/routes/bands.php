8498672ca7642bc6f629bc8246943a4e63801529
KapoMerjem <61578763+KapoMerjem@users.noreply.github.com>
2021-04-10 10:49:22 +0200

    dummy validation

--- api/routes/bands.php
+++ api/routes/bands.php
@@ -4,24 +4,21 @@ Flight::route('GET /bands', function(){
     $offset = Flight::query('offset', 0);
     $limit = Flight::query('limit', 10);
     $search = Flight::query('search');
-    
+
     Flight::json(Flight::bandService()->get_bands($search, $offset, $limit));
 });
 
 Flight::route('GET /bands/@id', function($id){
-    Flight::json(Flight::bandDao()->get_by_id($id));
+    Flight::json(Flight::bandService()->get_by_id($id));
 });
 
 Flight::route('POST /bands', function(){
-    $request = Flight::request()->data->getData();
-    Flight::json(Flight::bandDao()->add($data));
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::bandService()->add($data));
 });
 Flight::route('PUT /bands/@id', function($id){
-    $request = Flight::request();
-    $data = $request->data->getData();
-    Flight::bandDao()->update($id, $data);
-    $bands = Flight::bandDao()->get_by_id($id);
-    Flight::json($band);
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::bandService()->update($id, $data));
 });
 
 ?>
--- api/services/BandService.class.php
+++ api/services/BandService.class.php
@@ -1,10 +1,10 @@
 <?php
 
+require_once dirname (__FILE__).'/BaseService.class.php';
 require_once dirname(__FILE__).'/../dao/BandDao.class.php';
 
-class BandService{
+class BandService extends BaseService{
 
-  private $dao;
   public function __construct(){
     $this->dao = new BandDao();
   }
@@ -16,6 +16,13 @@ class BandService{
       return $this->dao->get_all($offset, $limit);
     }
   }
-}
 
+public function add($band){
+  //validation of account data
+  if(!isset($band['name'])) throw new Exception("Bla bla name is missing! ");
+
+  return parent::add($band);
+
+  }
+}
 ?>
