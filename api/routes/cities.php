edaaaf1949b40dd9f1ac9754824ee6c8c1c04a7c
KapoMerjem <61578763+KapoMerjem@users.noreply.github.com>
2021-06-28 10:48:46 +0200

    cities routes

--- api/routes/cities.php
+++ api/routes/cities.php
@@ -9,14 +9,19 @@
  */
 
 /**
- * @OA\Get(path="/cities",
+ * @OA\Get(path="/cities", tags={"cities"},
+ *     @OA\Parameter(type="integer",in="query", name="offset", default=0, description="Offset for pagination"),
+ *     @OA\Parameter(type="integer",in="query", name="limit", default=25, description="Limit for pagination"),
+ *     @OA\Parameter(type="integer",in="query", name="search", description="Search string for cities. Case insensitive search."),
+ *     @OA\Parameter(type="integer",in="query", name="order", default="-id", description="Sorting for return elements. -column_name ascending order by column_name or +column_name descending order by column_name"),
  *     @OA\Response(response="200", description="List cities from database")
+
  * )
  */
 
 Flight::route('GET /cities', function(){
     $offset = Flight::query('offset', 0);
-    $limit = Flight::query('limit', 10);
+    $limit = Flight::query('limit', 25);
     $search = Flight::query('search');
     $order = Flight::query('order', "-id");
 
