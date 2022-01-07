19f07a80b3bf85656e01d72b77f1956ee57023cf
KapoMerjem <61578763+KapoMerjem@users.noreply.github.com>
2022-01-07 10:10:05 +0100

    add get_user_by_email

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -13,6 +13,9 @@ class UserDao extends BaseDao{
   public function update_user_by_email($email, $user){
     $this->update("users", $email, $user, "email");
     }
+    public function get_user_by_email($email, $user){
+    return $this->query_unique("SELECT * FROM users WHERE email = :email", ["email" => $email]);
+  }
   public function get_user_by_token($token){
   return $this->query_unique("SELECT * FROM users WHERE token = :token", ["token" => $token]);
     }
